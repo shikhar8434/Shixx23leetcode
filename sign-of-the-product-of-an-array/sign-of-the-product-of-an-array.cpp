cd351c801f54e28d824b6bc9bc48798c7b8a7d24
Shikhar Srivastava <60563576+shikhar8434@users.noreply.github.com>
2021-07-16 01:29:15 +0530

    Time: 4 ms (93.18%), Space: 10.1 MB (84.72%) - LeetHub

---
+++ sign-of-the-product-of-an-array/sign-of-the-product-of-an-array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int arraySign(vector<int>& nums) {
+        
+        int prod=1;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]==0)
+            {
+                prod=0;
+            }
+            else if(nums[i]<0)
+            {
+                prod*=-1;
+            }
+        }
+        return prod;
+    }
+};
