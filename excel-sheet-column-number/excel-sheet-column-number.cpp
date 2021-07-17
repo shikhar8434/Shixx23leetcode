9ee03d31a2d044d33cfff9bbd80ddafb230a0d9c
Shikhar Srivastava <60563576+shikhar8434@users.noreply.github.com>
2021-07-17 15:19:04 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (61.06%) - LeetHub

---
+++ excel-sheet-column-number/excel-sheet-column-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int titleToNumber(string columnTitle) {
+        
+        int res=0,n=columnTitle.size();
+        for(int i=0;i<columnTitle.size();i++){
+            res=res*26+ (columnTitle[i]-'A'+1);
+        }
+        return res;
+    }
+};
+
